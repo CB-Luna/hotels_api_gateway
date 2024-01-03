31e40a177e0fd4e7cd577abf922d7b91fe8e0986
Uzziel Palma <uzzielpalma99@gmail.com>
2024-01-02 19:32:08 -0600

    Se corrige problema de CORS

--- api/api_gateway.py
+++ api/api_gateway.py
@@ -1,12 +1,22 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import os
+from flask_cors import CORS
 from datetime import datetime
 from scripts import sum_total_indicators
 
 app = Flask(__name__)
+CORS(app)  # Habilita CORS para toda la aplicación
 
-@app.route('/apigateway/update/sum_total_indicators/<int:document_id>', methods=['GET'])
+@app.route('/apigateway/update/sum_total_indicators/<int:document_id>',  methods=['GET', 'OPTIONS']) # Agregar método POST, DELETE, FETCH cuando se requiera
 def sum_total_indicator(document_id):
+    if request.method == 'OPTIONS':
+        response = make_response()
+        # Agregar headers correspondientes, dependiendo el método
+        response.headers['Access-Control-Allow-Methods'] = 'GET' # Agregar método POST, DELETE, FETCH cuando se requiera
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        return response
+    
     if request.method == 'GET':
         print("Calling to Sum Total Indicators")
         result,bool = sum_total_indicators.main(document_id)
